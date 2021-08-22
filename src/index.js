f00bae12c12d094f77197c8c090510fa5926bc13
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-08-22 15:45:27 +0300

    feat: refact code

--- src/index.js
+++ src/index.js
@@ -12,98 +12,84 @@ const priorities = {
     '/': 1,
 }
 
-function calculate(a, b, operator) {
-    switch (operator) {
-        case '+':
-            a += b;
-            break;
-        case '-':
-            a -= b;
-            break;
-        case '*':
-            a *= b;
-            break;  
-        case '/':
-            if (b === 0) {
-                throw new Error('TypeError: Division by zero.');
-            }
-            a /= b;
-            break;
-    }
-
-    return a;
+const calculate = (a, b, operator) => {
+  
+  switch (operator) {
+    case '+':
+      return  a += b;
+    case '-':
+      return a -= b;
+    case '*':
+      return a *= b;
+    case '/':
+      if (b === 0) {
+        throw new Error('TypeError: Division by zero.');
+      }
+      return a /= b; 
+  }
 }
 
-function getFirstPositionOfOperation(expression) {
-    return operators.reduce((acc, currentItem) => {
-        let indexOfItem = expression.indexOf(currentItem);
-        return (indexOfItem !== -1 && indexOfItem < acc) ? indexOfItem : acc;
-    }, expression.length);
+const replaceCalculatedResult = (arrOperator, output) => {
+  let lastOperator = arrOperator.pop();
+  let secondNumber = Number(output.pop());
+  let firstNumber = Number(output.pop());
+  return output.push(calculate(firstNumber, secondNumber, lastOperator));
 }
 
-function priorityIsHigher(stackOperation, newOperation) {
-    return priorities[stackOperation] >= priorities[newOperation];
+const getFirstPositionOperator = (expression) => {
+  const res = operators.reduce((acc, item) => {
+      let indexItem = expression.indexOf(item);
+      return (indexItem !== -1 && indexItem < acc) ? indexItem : acc;
+  }, expression.length);
+  return res;
 }
 
-function expressionCalculator(expr) {
+const isPriorityHigher  = (a, b) => {
+    return priorities[a] >= priorities[b];
+}
 
-    function replaceCalculatedResult() {
-        let lastStackOperation = stack.pop();
-        let secondNumner = Number(output.pop());
-        let firstNumber = Number(output.pop());
-        output.push(calculate(firstNumber, secondNumner, lastStackOperation));
-    }
+const expressionCalculator = (expr) => {
+  expr = expr.replace(/\s/g, '');
+  let output = [];
+  let stackOperators = [];
 
-    expr = expr.replace(/\s/g, '');
-
-    let output = [];
-    let stack = [];
-
-    while (expr) {
-
-        let firstOperationPos = getFirstPositionOfOperation(expr);
-    
-        if (firstOperationPos) {                //first symbol is number
-            let  number = expr.slice(0, firstOperationPos);
-            output.push(number);
-            expr = expr.slice(firstOperationPos, expr.length);
-        } else {                                //first symbol is operation
-            let operation = expr[0];
-
-            if (operation === ')') {
-
-                if (!(stack.includes('('))) throw new Error('ExpressionError: Brackets must be paired');
-
-                while (stack[stack.length - 1] !== '(') {
-                    replaceCalculatedResult();
-                }
-                
-                stack.pop();
-
-            } else {
-
-                if (!(operation === '(')) {
-
-                    while (stack.length && priorityIsHigher(stack[stack.length - 1], operation)) {
-                        replaceCalculatedResult();
-                    }
-
-                }
-
-                stack.push(operation);
-            }
-            
-            expr = expr.slice(1, expr.length);
+  while (expr) {
+    let firstOperatorPos = getFirstPositionOperator(expr);
+    if (firstOperatorPos) {
+      let  number = expr.slice(0, firstOperatorPos);
+      output.push(number);
+      expr = expr.slice(firstOperatorPos, expr.length);
+    } else {
+      let operator = expr[0];
+      if (operator === ')') {
+        if (!(stackOperators.includes('('))) {
+          throw new Error('ExpressionError: Brackets must be paired');
         }
+        while (stackOperators[stackOperators.length - 1] !== '(') {
+          replaceCalculatedResult(stackOperators, output);
+        }
+        stackOperators.pop();
+      } else {
+        if (operator !== '(') {
+          while (stackOperators.length && isPriorityHigher (stackOperators[stackOperators.length - 1], operator)) {
+            replaceCalculatedResult(stackOperators, output);
+          }
+        }
+        stackOperators.push(operator);
+      }
+      expr = expr.slice(1, expr.length);
     }
+  }
 
-    if (output.includes(NaN) || stack.includes('(')) throw new Error('ExpressionError: Brackets must be paired');
+  if (output.includes(NaN) || stackOperators.includes('(')) {
+      throw new Error('ExpressionError: Brackets must be paired');
+  }
 
-    while (stack.length) {
-        replaceCalculatedResult();
-    }
-
-    return output[0];
+  while (stackOperators.length) {
+    replaceCalculatedResult(stackOperators, output);
+  }
+  const res = output[0];
+  return res;
 }
 
 module.exports = {
